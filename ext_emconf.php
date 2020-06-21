05382524010e5fdeb76d51ead8d2ae348182f946
Susanne Moog <look@susi.dev>
2020-06-21 10:08:59 +0200

    [TASK]Emconf

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'System Status Example',
+    'description' => '',
+    'category' => 'examples',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Susanne Moog',
+    'author_email' => 'susanne.moog@typo3.com',
+    'version' => '10.4.x-dev',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
